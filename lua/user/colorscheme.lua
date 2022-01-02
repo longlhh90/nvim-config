0d8bfb28b503029255f632a639a9d012cc4c8711
Lukas <longlhh90@gmail.com>
2022-01-02 00:21:34 -0600

    init commit

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,12 @@
+local colorscheme = "kanagawa"
+
+require("kanagawa").setup({
+  transparent = true,
+})
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
