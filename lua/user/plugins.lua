f13c0880effa8f42dfd6cf1e8b44d37d0f0d3274
Lukas <longlhh90@gmail.com>
2022-01-16 22:14:01 -0600

    add null-ls format

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,7 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" --simple to use language server installer
   use "ray-x/lsp_signature.nvim"
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
