c9a85cc50506779bdc45e52095c166119ec87b32
Lukas <longlhh90@gmail.com>
2022-08-27 23:52:14 -0600

    update keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -72,6 +72,12 @@ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+-- Do not yank with x
+keymap("n", "x", '"_x', opts)
+
+-- Select all
+keymap("n", "<C-a>", "gg<S-v>G", opts)
+
 -- Telescope
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 --keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false}))<cr>", opts)
