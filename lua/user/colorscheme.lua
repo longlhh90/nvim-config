ca8a53c7ac4b3b190c37e66456f9257eb0f86b30
Lukas <longlhh90@gmail.com>
2022-08-27 23:52:07 -0600

    update with global status

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,23 @@
 local colorscheme = "kanagawa"
 
+vim.opt.laststatus = 3
+vim.opt.fillchars:append({
+  horiz = "━",
+  horizup = "┻",
+  horizdown = "┳",
+  vert = "┃",
+  vertleft = "┨",
+  vertright = "┣",
+  verthoriz = "╋",
+})
+
 require("kanagawa").setup({
+  globalStatus = true,
   transparent = true,
 })
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
 end
